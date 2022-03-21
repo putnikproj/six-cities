23d0abb18a198b3ccbac3be574bc7dd693b5a494
putnik-projects <53861888+putnik-projects@users.noreply.github.com>
2022-03-21 11:43:15 +0300

    Adds "header" component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__left">
+            <Link className="header__logo-link header__logo-link--active" to={AppRoute.ROOT}>
+              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+            </Link>
+          </div>
+          <nav className="header__nav">
+            <ul className="header__nav-list">
+              <li className="header__nav-item user">
+                <a className="header__nav-link header__nav-link--profile" href="#todo">
+                  <div className="header__avatar-wrapper user__avatar-wrapper">
+                  </div>
+                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                </a>
+              </li>
+              <li className="header__nav-item">
+                <a className="header__nav-link" href="#todo">
+                  <span className="header__signout">Sign out</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- src/components/not-found-page/not-found-page.tsx
+++ src/components/not-found-page/not-found-page.tsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../const';
+import Header from '../header/header';
 
 function NotFoundPage(): JSX.Element {
   return (
     <>
+      <Header />
       <h1>404. Page not found</h1>
       <Link to={AppRoute.ROOT}>Go to main page</Link>
     </>
