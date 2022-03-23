3ec8cb5530e9378bd2cc472bd990b1b794f1b24e
putnik-projects <53861888+putnik-projects@users.noreply.github.com>
2022-03-23 16:47:57 +0300

    Extracts "star" in "add-review-form"

--- src/components/add-review-form/add-review-form.tsx
+++ src/components/add-review-form/add-review-form.tsx
@@ -12,45 +12,36 @@ function AddReviewForm() {
     setScore(Number(target.value));
   }
 
+  // Star component
+  type StarProps = {
+    value: number,
+    title: string,
+  }
+  function Star({value, title}: StarProps) {
+    return (
+      <>
+        <input className="form__rating-input visually-hidden" name="rating" value={value} id={`${value}-stars`} type="radio" checked={score === value} onChange={handleRadioButtonChange} />
+        <label htmlFor={`${value}-stars`} className="reviews__rating-label form__rating-label" title={title}>
+          <svg className="form__star-image" width="37" height="33">
+            <use xlinkHref="#icon-star"></use>
+          </svg>
+        </label>
+      </>
+    );
+  }
+
   return (
     <form className="reviews__form form" action="#" method="post">
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
+
       <div className="reviews__rating-form form__rating">
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" checked={score === 5} onChange={handleRadioButtonChange} />
-        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" checked={score === 4} onChange={handleRadioButtonChange} />
-        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" checked={score === 3} onChange={handleRadioButtonChange} />
-        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" checked={score === 2} onChange={handleRadioButtonChange} />
-        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"  checked={score === 1} onChange={handleRadioButtonChange} />
-        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
+        <Star value={5} title="perfect" />
+        <Star value={4} title="good" />
+        <Star value={3} title="not bad" />
+        <Star value={2} title="badly" />
+        <Star value={1} title="terribly" />
       </div>
+
       <textarea
         className="reviews__textarea form__textarea"
         id="review"
@@ -60,12 +51,14 @@ function AddReviewForm() {
         value={reviewText}
       >
       </textarea>
+
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
         </p>
         <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
       </div>
+
     </form>
   );
 }
