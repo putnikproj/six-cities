cee22ee43ed810e7f3d1a1064d59c663804f45e1
putnik-projects <53861888+putnik-projects@users.noreply.github.com>
2022-03-27 12:47:25 +0300

    Decomposes "login-form" in "login-page"

--- src/components/login-page/login-page.tsx
+++ src/components/login-page/login-page.tsx
@@ -1,4 +1,34 @@
+import { ChangeEvent, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 import Header from '../header/header';
+
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleLoginInputChange({ target }: ChangeEvent<HTMLInputElement>) {
+    setEmail(target.value);
+  }
+
+  function handlePasswordInputChange({ target }: ChangeEvent<HTMLInputElement>) {
+    setPassword(target.value);
+  }
+
+  return (
+    <form className="login__form form" action="#" method="post">
+      <div className="login__input-wrapper form__input-wrapper">
+        <label className="visually-hidden">E-mail</label>
+        <input className="login__input form__input" value={email} onChange={handleLoginInputChange} type="email" name="email" placeholder="Email" required />
+      </div>
+      <div className="login__input-wrapper form__input-wrapper">
+        <label className="visually-hidden">Password</label>
+        <input className="login__input form__input" value={password} onChange={handlePasswordInputChange}  type="password" name="password" placeholder="Password" required />
+      </div>
+      <button className="login__submit form__submit button" type="submit">Sign in</button>
+    </form>
+  );
+}
 
 function LoginPage(): JSX.Element {
   return (
@@ -7,27 +37,20 @@ function LoginPage(): JSX.Element {
 
       <main className="page__main page__main--login">
         <div className="page__login-container container">
+
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
-              <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required />
-              </div>
-              <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required />
-              </div>
-              <button className="login__submit form__submit button" type="submit">Sign in</button>
-            </form>
+            <LoginForm />
           </section>
+
           <section className="locations locations--login locations--current">
             <div className="locations__item">
-              <a className="locations__item-link" href="/">
+              <Link className="locations__item-link" to="/">
                 <span>Amsterdam</span>
-              </a>
+              </Link>
             </div>
           </section>
+
         </div>
       </main>
     </div>
