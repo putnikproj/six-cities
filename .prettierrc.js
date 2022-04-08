28c670a2c2d4144fb26952207ac800fc11864911
putnik-projects <53861888+putnik-projects@users.noreply.github.com>
2022-04-08 14:48:12 +0300

    Adds prettier to project

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+// More at https://prettier.io/docs/en/options.html
+
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+  singleAttributePerLine: false,
+};
