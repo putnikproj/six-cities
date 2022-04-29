84bf2328fa19db4ea9df17809c69d656a8bb916b
putnik-projects <53861888+putnik-projects@users.noreply.github.com>
2022-04-29 21:01:24 +0300

    Decomposes 'CitiesEmpty' component

---
+++ src/components/cities/cities-empty.tsx
@@ -0,0 +1,25 @@
+import { CityName } from '../../helpers/enum';
+
+type CitiesEmptyProps = {
+  activeCity: CityName;
+};
+
+function CitiesEmpty({ activeCity }: CitiesEmptyProps) {
+  return (
+    <div className="cities">
+      <div className="cities__places-container cities__places-container--empty container">
+        <section className="cities__no-places">
+          <div className="cities__status-wrapper tabs__content">
+            <b className="cities__status">No places to stay available</b>
+            <p className="cities__status-description">
+              We could not find any property available at the moment in {activeCity}
+            </p>
+          </div>
+        </section>
+        <div className="cities__right-section"></div>
+      </div>
+    </div>
+  );
+}
+
+export default CitiesEmpty;
--- src/components/cities/cities.tsx
+++ src/components/cities/cities.tsx
@@ -3,9 +3,10 @@ import { CityName, LoadStatus } from '../../helpers/enum';
 import { Offer } from '../../types';
 import { offerToPoint } from '../../helpers/util';
 
-import CitiesPlaceList from '../cities-place-list';
 import Map from '../map';
 import Spinner from '../spinner';
+import CitiesEmpty from './cities-empty';
+import CitiesPlaceList from '../cities-place-list';
 
 type CitiesProps = {
   offers: Offer[];
@@ -24,21 +25,7 @@ function Cities({ offers, activeCity }: CitiesProps) {
   }
 
   if (offers.length === 0) {
-    return (
-      <div className="cities">
-        <div className="cities__places-container cities__places-container--empty container">
-          <section className="cities__no-places">
-            <div className="cities__status-wrapper tabs__content">
-              <b className="cities__status">No places to stay available</b>
-              <p className="cities__status-description">
-                We could not find any property available at the moment in {activeCity}
-              </p>
-            </div>
-          </section>
-          <div className="cities__right-section"></div>
-        </div>
-      </div>
-    );
+    return <CitiesEmpty activeCity={activeCity} />;
   }
 
   return (
